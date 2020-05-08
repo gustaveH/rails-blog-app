ea3aefbe1d555c2214176510528733697cb02803
gustaveH <ghabiman@gmail.com>
2020-05-08 13:41:09 -0400

    Added the update and edit for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,15 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    def edit
+        @user = User.find(params[:id])
+    end
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+            flash[:notice]= "your account information was successfully updated"
+            redirect_to articles_path
+    end
 
     def create
         @user = User.new(user_params)
