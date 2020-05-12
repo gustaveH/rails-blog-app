2fae6c0158253b679428f4ec64d9ec5da9dd5ebc
gustaveH <ghabiman@gmail.com>
2020-05-12 11:34:09 -0400

    Updated user profiles,Article index, andarticle show page to display image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,12 @@ class UsersController < ApplicationController
     end
     def update
         @user = User.find(params[:id])
-        @user.update(user_params)
-            flash[:notice]= "your account information was successfully updated"
-            redirect_to articles_path
+        if @user.update(user_params)
+            flash[:notice]= "Your account information was successfully updated"
+            redirect_to @user
+        else 
+            render "edit"
+        end
     end
 
     def create
