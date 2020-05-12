597a34b66ab4960d03ff7b2cddeb0be9615dc20e
gustaveH <ghabiman@gmail.com>
2020-05-12 16:17:19 -0400

    Restrict actions for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user,  only: [:edit, :update]
+
     def show
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
@@ -40,6 +43,12 @@ class UsersController < ApplicationController
     end
     def set_user
         @user = User.find(params[:id])
+    end
+
+    def require_same_user
+        if current_user !=@user
+            flash[:alert] = "You do not have access"
+        end
     end
 
 end
