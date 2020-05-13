d11e80c14952dab7744d5da2dc1b163e53f70be7
gustaveH <ghabiman@gmail.com>
2020-05-13 14:58:54 -0400

    Added adminuser access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
     end
     def destroy
         @user.destroy
-        session[:user_id] = nil
+        session[:user_id] = nil if @user == current_user
         flash[:notice] = "Account and articles deleted"
         redirect_to articles_path
     end
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-        if current_user !=@user
+        if current_user !=@user && !current_user.admin?
             flash[:alert] = "You do not have access"
         end
     end
